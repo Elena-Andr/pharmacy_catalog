596a691eed3aaca6ef3967cbe61f61df988bd057
Elena-Andr <jopinet@gmail.com>
2016-06-05 02:44:03 +0600

    initial commit

---
+++ app/src/main/java/com/android/pharmacycatalogfragments/Utility/DateTimeHelper.java
@@ -0,0 +1,52 @@
+package com.android.pharmacycatalogfragments.Utility;
+
+import android.content.res.Resources;
+import android.util.Log;
+
+import com.android.pharmacycatalogfragments.R;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateTimeHelper {
+    private static String LOG_TAG = DateTimeHelper.class.getSimpleName();
+
+    public static String getCurrentDateAsString() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat(Resources.getSystem().getString(R.string.date_format));
+        return dateFormat.format(calendar);
+    }
+
+    public static Calendar getCurrentDateAsCalendar() {
+        return Calendar.getInstance();
+    }
+
+    public static Calendar getCalendarFromString(String date, String dateFromat) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(dateFromat);
+        Date d = null;
+        try {
+            d = dateFormat.parse(date);
+        } catch (ParseException e) {
+            Log.e(LOG_TAG, "Error ", e);
+        }
+
+        Calendar thatDay = Calendar.getInstance();
+        thatDay.setTime(d);
+
+        return thatDay;
+    }
+
+    public static int getMinutesBetweenDates(Calendar c1, Calendar c2){
+        long diff = c1.getTimeInMillis() - c2.getTimeInMillis();
+        int minutes = (int) ((diff / (1000*60)) % 60);
+
+        return minutes;
+    }
+
+    public static String getStringFromCalendar(Calendar calendar, String dateFormat) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+        return simpleDateFormat.format(calendar);
+    }
+}
