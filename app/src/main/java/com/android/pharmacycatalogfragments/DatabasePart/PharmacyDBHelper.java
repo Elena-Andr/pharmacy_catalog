2071a4294344c2d6dcf6f0ec4dd0df9ad69fc544
Elena-Andr <jopinet@gmail.com>
2016-06-26 22:39:07 +0600

    minor changes

--- app/src/main/java/com/android/pharmacycatalogfragments/DatabasePart/PharmacyDBHelper.java
+++ app/src/main/java/com/android/pharmacycatalogfragments/DatabasePart/PharmacyDBHelper.java
@@ -1,21 +1,12 @@
 package com.android.pharmacycatalogfragments.DatabasePart;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.net.Uri;
-import android.util.Log;
 
 import com.android.pharmacycatalogfragments.DatabasePart.PharmacyContract.CatalogEntry;
-import com.android.pharmacycatalogfragments.Utility.CSVParser;
-import com.android.pharmacycatalogfragments.Utility.DownloadFileFromURL;
-
-import java.io.IOException;
 
 public class PharmacyDBHelper extends SQLiteOpenHelper {
-
-    private static final String LOG_TAG = SQLiteOpenHelper.class.getSimpleName();
 
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "pharmacy.db";
@@ -44,45 +35,7 @@ public class PharmacyDBHelper extends SQLiteOpenHelper {
                         CatalogEntry.COLUMN_MODIFIED_DATE + TEXT_TYPE_NOT_NULL + " )";
 
         sqLiteDatabase.execSQL(SQL_CREATE_CATALOG_TABLE);
-
-        // TODO: used just for tests, should be removed
-       // fillWithInitialData(sqLiteDatabase);
     }
-
-    // THIS METHOD IS ONLY FOR TESTS, WILL BE REMOVED LATER
-    /*private void fillWithInitialData(SQLiteDatabase sqLiteDatabase) {
-        String urlPath = "http://vk.com/doc340921770_437488672?hash=bf70739b445672e04c&dl=f9f924d4b4e9a38b8d";
-        String localFilePath;
-        try {
-            localFilePath = DownloadFileFromURL.downloadFile(urlPath, context);
-            ContentValues[] contentValues = CSVParser.Parse( localFilePath);
-
-            if(contentValues.length > 0) {
-                bulkInsert(PharmacyContract.CONTENT_URI, contentValues, sqLiteDatabase);
-            }
-
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "ERROR", e);
-        }
-    }
-
-    public int bulkInsert(Uri uri, ContentValues[] values, SQLiteDatabase db) {
-                db.beginTransaction();
-                int returnCount = 0;
-                try {
-                    for (ContentValues value : values) {
-                        long _id = db.insert(PharmacyContract.CatalogEntry.TABLE_NAME, null, value);
-                        if (_id != -1) {
-                            returnCount++;
-                        }
-                    }
-                    db.setTransactionSuccessful();
-                } finally {
-                    db.endTransaction();
-                }
-                context.getContentResolver().notifyChange(uri, null);
-                return returnCount;
-        }*/
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
